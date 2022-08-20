508f789645b8f9475fcd8a10bc66d21303f1079c
nahidpavelc <nahidpavelchowdhury777@hotmail.com>
2022-08-20 16:44:58 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
+import Aboutus from './Components/Aboutus/Aboutus';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header/>
+
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/Home" element={<Home/>} />
+          <Route exact path="/Aboutus" element={<Aboutus/>} />
+        </Routes>
+
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
