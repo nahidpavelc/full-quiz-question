44efa80e250bbc633b123ec4948b7e6db0f5a1e8
nahidpavelc <nahidpavelchowdhury777@hotmail.com>
2022-08-25 09:43:55 +0600

    New Update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
+    <div className='' style={{height:'700px'}}>
       <h1>Home</h1>
     </div>
   );
--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import navLogo from "../../../Assets/img/Logo_Final_Wide.png";
+import "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg bg-light" style={{p:''}}>
+        <div class="container-fluid container">
+          <a class="navbar-brand" href="#" style={{fontweight:'700'}} >
+            <img className="me-2" src={navLogo} alt="" width="auto" height="60" style={{}}/>
+            {/* QUIZ */}
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <div class="d-flex" role="search">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a>
+                    <NavLink className="nav-link" to="/Home">
+                      {" "}
+                      Home
+                    </NavLink>
+                  </a>
+                </li>
+
+                <li class="nav-item">
+                  <a>
+                    <NavLink className="nav-link" to="/products">
+                      {" "}
+                      Quiz
+                    </NavLink>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a>
+                    <NavLink className="nav-link" to="/Contactus">
+                      {" "}
+                      Contact us
+                    </NavLink>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
