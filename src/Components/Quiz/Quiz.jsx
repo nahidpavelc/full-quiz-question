074f02fe2bc611bbe1ad466726e6aab7c514223f
nahidpavelc <nahidpavelchowdhury777@hotmail.com>
2022-08-30 14:50:40 +0600

    new Commit

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -11,19 +11,59 @@ const Quiz = () => {
       .then(data => setQuizes(data))
   }, []);
 
-  return (
-    <div style={{ height: '700px' }}>
-      <div className="container">
-        <h1 class="card-title  banner-txt mb-1"> <span className="text-success">Biology </span>Quiz Exam </h1>
-        <div className='card-title-h1 mb-5 pt-0'></div>
+  // Set the date we're counting down to
+  var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          {
-            Quizes.map(Quiz => <Qunumber
-              key={Quiz.id}
-              Quiz={Quiz}
-            ></Qunumber>)
-          }
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 40 * 3 )) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  return (
+    <div style={{ height: 'Auto' }}>
+      <div className="container card my-5" style={{ backgroundColor: '#E6E0CB' }}>
+        <div className='container'>
+          <div className='card-un p-2'>
+            <h1 class="card-title p-4 banner-txt"> <span className="text-success">Biology </span> Exam </h1>
+            <div className='d-flex justify-content-between'>
+              <div>
+                <p className='px-3 mb-0' id="demo" style={{fontWeight:700}}></p>
+              </div>
+              <div className='text-start'>
+                <p className='px-3 mb-0' style={{fontWeight:700}}>Full Marks: 40</p>
+                <p className='px-3 mb-0' style={{fontWeight:700}}>Total Questions: {Quizes.length}</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row row-cols-1 row-cols-md-3 g-4 pb-5 mt-0" >
+            {
+              Quizes.map(Quiz => <Qunumber
+                key={Quiz.id}
+                Quiz={Quiz}
+              ></Qunumber>)
+            }
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Quiz/Qunumber.jsx
+++ src/Components/Quiz/Qunumber.jsx
@@ -8,15 +8,15 @@ const Qunumber = ({ Quiz }) => {
   return (
     <div>
       <div className="col">
-        <div className="card h-100">
+        <div className="card h-100 cardq" style={{backgroundColor:'#E6E0CB'}}>
           {/* <img className="card-img-top" alt="..." /> */}
           <div className="card-body">
             <p className="card-text text-start" style={{ fontSize: '20px' }}>{id}. {que}</p>
           </div>
           <div>
 
-            <tr >
-              <td className='px-4'><label class="containerq">{ans1}
+            <tr>
+              <td className='px-4 ' style={{}}><label class="containerq">{ans1}
                 <input type="checkbox" />
                 <span class="checkmark"></span>
               </label></td>
